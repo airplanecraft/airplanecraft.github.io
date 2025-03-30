6d8ed41359c11c9a0e4c5041fd9a0ea5c3bdf58a
airplanecraft <airplanecraft@users.noreply.github.com>
2025-03-30 08:50:59 +0000

    auto deploy airplanecraft/website-airplanecraft-code@7b7ead7aa4ac6f0a17f6e798a6ccc39469852267

--- zh/js/bundle.8e94ce198bad5728039e658851505246f00a949fd90aa866e2bc920f5b7bb5a94c291e484fc8e502c717f7ae5e329ab8e6e54902a9bcdde5c5239d1520fcd952.js
+++ zh/js/bundle.8e94ce198bad5728039e658851505246f00a949fd90aa866e2bc920f5b7bb5a94c291e484fc8e502c717f7ae5e329ab8e6e54902a9bcdde5c5239d1520fcd952.js
@@ -9,7 +9,7 @@ const pageHasLoaded = 'DOMContentLoaded';
 const imageAltClass = 'img_alt';
 
 
-const defaultSiteLanguage = 'en';
+const defaultSiteLanguage = 'zh';
 const baseURL = 'https://www.airplanecraft.com/';
 const searchFieldClass = '.search_field';
 const searchClass = '.search';
@@ -17,11 +17,11 @@ const goBackClass = 'button_back';
 const lineClass = '.line';
 
 // defined in i18n / translation files
-const quickLinks = 'Results';
-const searchResultsLabel = 'Search Results';
-const shortSearchQuery = 'Query is too short'
-const typeToSearch = 'Type to search';
-const noMatchesFound = 'No results found';
+const quickLinks = '搜索到的结果';
+const searchResultsLabel = '搜索到的结果';
+const shortSearchQuery = '搜索字符太短'
+const typeToSearch = '请输入内容查找';
+const noMatchesFound = '未找到结果';
 
 ;
 // global variables
